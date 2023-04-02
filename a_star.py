25c0cd03d5bd2e27edc43630f5f164dc297fa8f7
Shubh Mall <mall.shubhkumar@gmail.com>
2023-04-01 18:03:12 -0700

    Finished implementation of drawing the shortest path. Algorithm keeps track fo the f score and path to determine the shortest path. Can use key c to restart/refresh the game

--- a_star.py
+++ a_star.py
@@ -15,7 +15,7 @@
 #                               Includes                                      #
 ###############################################################################
 import pygame
-import math
+import time
 from queue import PriorityQueue
 
 ###############################################################################
@@ -148,8 +148,60 @@ def h(n1, n2):
     x2, y2 = n2
     return abs(x1-x2) + abs(y1-y2)
 
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+
 def algorithm(draw, grid, start, end):
-    pass
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {node: float("inf") for row in grid for node in row}
+    g_score[start] = 0
+    f_score = {node: float("inf") for row in grid for node in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    # To keep track of nodes in the priority queue
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        # We found the shortest path
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True
+
+        for neighbour in current.neighbours:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbour]:
+                came_from[neighbour] = current
+                g_score[neighbour] = temp_g_score
+                f_score[neighbour] = temp_g_score + h(neighbour.get_pos(), end.get_pos())
+                if neighbour not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbour], count, neighbour))
+                    open_set_hash.add(neighbour)
+                    neighbour.make_open()
+
+        # Sleep to slow down visualization
+        #time.sleep(0.05)
+        draw()
+
+        if current != start:
+            current.make_closed()
+
+    return False
 
 # Make a square grid with given rows and total width
 def make_grid(rows, width):
@@ -207,16 +259,12 @@ def main(win, width):
     end = None
 
     run = True
-    started = False
 
     while run:
         draw(win, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-            if started:
-                continue
 
             # Left click
             if pygame.mouse.get_pressed()[0]:
@@ -244,12 +292,17 @@ def main(win, width):
                     end = None
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for node in row:
-                            node.update_neighbours()
+                            node.update_neighbours(grid)
 
                     algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
 
     pygame.quit()
 
